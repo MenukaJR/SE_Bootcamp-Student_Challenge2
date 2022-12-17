5e56fd5e0ead0056aa123723b1da144e3ff418a3
MenukaJR <120049194+MenukaJR@users.noreply.github.com>
2022-12-17 22:12:29 +0530

    Added somewhat working comparison mechanism

---
+++ Student Challenge 2/STCH2.java
@@ -0,0 +1,39 @@
+public class STCH2 {
+    static String[] txtAR;
+    static int i = 0, score;
+    static char q;
+
+    public static void InputString(String text) {
+        txtAR = new String[text.length()];
+
+        for (int y = 0; y <= text.length(); y++) {
+            i++;
+            score = 0;
+            if (i <= text.length()) {
+                txtAR[y] = text.substring(y, i);
+                // System.out.println(text.charAt(y));// add this to a stack
+
+                if (txtAR[y].equals("{") || txtAR[y].equals("[") || txtAR[y].equals("("))
+                    for (int n = 1; n <= text.length() - 1; n++) {
+
+                        char g = text.charAt(n);
+
+                        if (g == '}' || g == ']' || g == ')') {
+                            System.out.println("Input Match !!");
+                            score = 0;
+                        } else {
+                            score++;
+                            System.out.println("Input Misatch !! Score: " + score + " char: " + g);
+                        }
+                        System.out.println("Final Score: " + score + " char: " + g);
+
+                    }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        InputString("{fvffvfv]fvfv}");
+    }
+}
