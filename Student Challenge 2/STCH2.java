71828790332f9b7488db153233a564ae5c26dd19
MenukaJR <120049194+MenukaJR@users.noreply.github.com>
2022-12-17 23:59:43 +0530

    ubdated comparison mechanism

--- Student Challenge 2/STCH2.java
+++ Student Challenge 2/STCH2.java
@@ -1,47 +1,48 @@
 public class STCH2 {
     static String[] txtAR;
-    static int i = 0, score;
+    static int i, score,count;
     static char q;
 
     public static void InputString(String text) {
         txtAR = new String[text.length()];
-
+        i = 0;
+        score = 0;
+        count = 0;
+        
         for (int y = 0; y <= text.length(); y++) {
             i++;
-            score = 0;
+
             if (i <= text.length()) {
                 txtAR[y] = text.substring(y, i);
                 // System.out.println(text.charAt(y));// add this to a stack
 
-                if (txtAR[y].equals("{") || txtAR[y].equals("[") || txtAR[y].equals("("))
-                    for (int n = 1; n <= text.length() - 1; n++) {
-
+                if (txtAR[y].equals("{") || txtAR[y].equals("[") || txtAR[y].equals("("))// finding an opening bracket
+                count=y; 
+                count++; 
+                for (int n = 0; n <= text.length()-1; n++) {
+                        
                         char g = text.charAt(n);
-
-                        if (g == '}') {
-                            System.out.println("Input Match !!");
-                            
-                        } else {
+                        //String g = txtAR[n].toString();
+                        //System.out.println(g);
+                        if(g=='}'||g==']'||g==')'){//checking for close brackets
+                            //System.out.println("Input match !!");
+                            score = 0;
+                        }else{
                             score++;
-                            System.out.println("Input Misatch !! Score: " + score + " char: " + g);
                         }
-
-                        if (g == ']') {
-                            System.out.println("Input Match !!");
-                            
-                        } else {
-                            score++;
-                            System.out.println("Input Misatch !! Score: " + score + " char: " + g);
-                        }
-                        System.out.println("Final Score: " + score + " char: " + g);
-
                     }
             }
+        }
+        System.out.println("Final Score: " + score);
+        if(score>=0){
+            System.out.println("Input mismatch");
+        }else{
+            System.out.println("Input Match");
         }
 
     }
 
     public static void main(String[] args) {
-        InputString("{{}]}");
+        InputString("{}[]");
     }
 }
